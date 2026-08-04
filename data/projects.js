abcd2d271f4b402546503fcc36e91969721c110a
Remox-code <remoxafficial.4@gmail.com>
2026-08-04 11:25:04 +0330

    Update website

--- data/projects.js
+++ data/projects.js
@@ -1,7 +1,7 @@
 export const projects = [
   {
     title: "Remox Games",
-    image: "image/projects/Remox-games.png",
+    image: "image/projects/Remox-games.webp",
     description:
       "🎮 مجموعه‌ای از بازی‌ها و سرگرمی‌های جذاب که با JavaScript توسعه داده شده‌اند؛ با تمرکز بر تجربه کاربری روان، انیمیشن‌های پویا و تعامل بالا",
     tags: ["HTML", "CSS", "JavaScript"],
@@ -10,7 +10,7 @@ export const projects = [
 
   {
     title: "Kesht Sanat Hamed",
-    image: "image/projects/kesht-sanat.png",
+    image: "image/projects/kesht-sanat.webp",
     description:
       "🌱 وب‌سایت معرفی محصولات کشاورزی و رستوران با طراحی مدرن، صفحات کاملاً واکنش‌گرا و رابط کاربری ساده برای دسترسی سریع کاربران",
     tags: ["HTML", "CSS", "JavaScript"],
@@ -19,7 +19,7 @@ export const projects = [
 
   {
     title: "Remox Store",
-    image: "image/projects/Remox-store.png",
+    image: "image/projects/Remox-store.webp",
     description:
       "🛍️ فروشگاه اینترنتی انواع اکسسوری با طراحی شیک، دسته‌بندی محصولات و تجربه خریدی سریع و لذت‌بخش برای کاربران",
     tags: ["HTML", "CSS", "JavaScript"],
@@ -28,7 +28,7 @@ export const projects = [
 
   {
     title: "Remox Gym",
-    image: "image/projects/Remox-gym.png",
+    image: "image/projects/Remox-gym.webp",
     description:
       "💪 وب‌سایت باشگاه بدنسازی با امکاناتی مانند معرفی خدمات، ثبت‌نام، پرداخت شهریه و دریافت برنامه‌های تمرینی به‌صورت آنلاین",
     tags: ["HTML", "CSS", "JavaScript"],
@@ -37,7 +37,7 @@ export const projects = [
 
   {
     title: "Boosty",
-    image: "image/projects/boosty.png",
+    image: "image/projects/boosty.webp",
     description:
       "⚡ یک ابزار آنلاین با رابط کاربری مدرن که دسترسی سریع به امکانات کاربردی را فراهم کرده و تجربه‌ای روان برای کاربران ایجاد می‌کند",
     tags: ["HTML", "CSS", "JavaScript"],
@@ -46,7 +46,7 @@ export const projects = [
 
   {
     title: "Remox Tracker",
-    image: "image/projects/Remox-tracker.png",
+    image: "image/projects/Remox-tracker.webp",
     description:
       "📊 ابزار مدیریت و توسعه فردی برای برنامه‌ریزی، پیگیری اهداف و ثبت فعالیت‌های روزانه با محیطی ساده و کاربردی",
     tags: ["HTML", "CSS", "JavaScript"],
